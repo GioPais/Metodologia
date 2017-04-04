70592003e94c9dc32339a503baee5ffa285d5a64
Giovanni Pais <gio.pais@hotmail.com>
2017-04-04 04:10:32 -0300

    Se completa ejemplo cachipun

---
+++ cc3002/src/cachipun/Paper.java
@@ -0,0 +1,21 @@
+package cachipun;
+
+public class Paper {
+
+	public int play(Hand v){
+		return v.playWithPaper(this);
+	}
+	
+	public int playWithStone(Stone v){
+		return 1;
+	}
+	
+	public int playWithScissor(Scissor v){
+		return -1;
+	}
+	
+	public int playWithPaper(Paper v){
+		return 0;
+	}
+	
+}
