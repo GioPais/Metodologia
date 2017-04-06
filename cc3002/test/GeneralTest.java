a153b688cdce1fc8241574a117d3070b6c62a57f
giovanni <giovanni@Elucidator>
2017-04-06 12:26:11 -0300

    Se sube el GeneralTest correcto.

--- cc3002/test/GeneralTest.java
+++ cc3002/test/GeneralTest.java
@@ -10,24 +10,40 @@ import org.junit.Test;
 public class GeneralTest {
 	
 	private Fire f;
+	private Fire f2;
 	private Grass gs;
 	
 	@Before
 	public void setUp() throws Exception {
 		f = new Fire("torchic",100,10);
+		f2 = new Fire("Charmander",100,10);
 		gs = new Grass("bulbasaur",100,10);
 	}
 	
 	
+	//@Test
+	//public void test() {
+	//	fail("Not yet implemented");
+	//}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAtack(){
+		f.atack(f2);
+		assertEquals(10,f2.damage,0);
+		f2.heal();
 	}
 	
 	@Test
-	public void test(){
+	public void testAtackRes(){
+		gs.atack(f);
+		assertEquals(-10,f.damage,0);
+	}
+	
+	@Test
+	public void testAtackVul(){
 		f.atack(gs);
-		asserEquals(20,gs.damage,0.1);
+		assertEquals(20,gs.damage,0);
+		gs.heal();
 	}
 
 }
