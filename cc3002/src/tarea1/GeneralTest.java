db00355584b5e4f7149b062f9f23a71318a2f42f
giovanni <giovanni@192.168.1.35>
2017-04-06 03:57:52 -0300

    Se agrega GeneralTest al package principal para correr test

---
+++ cc3002/src/tarea1/GeneralTest.java
@@ -0,0 +1,33 @@
+package tarea1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.Test;
+
+public class GeneralTest {
+	
+	private Fire f;
+	private Grass gs;
+	
+	@Before
+	public void setUp() throws Exception {
+		f = new Fire("torchic",100,10);
+		gs = new Grass("bulbasaur",100,10);
+	}
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAtack(){
+		f.atack(gs);
+		assertEquals(20,gs.damage,0.1);
+	}
+
+}
