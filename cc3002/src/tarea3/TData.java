7559eb56bcfa2c19f5587e086763c76615c1a4ca
Giopa <Giopa@DESKTOP-S7VEV72.iptv.microsoft.com>
2017-06-27 15:47:29 -0400

    "Se agregan metodos para parsear Tdata. Aun incompleto"

--- cc3002/src/tarea3/TData.java
+++ cc3002/src/tarea3/TData.java
@@ -1,5 +1,32 @@
 package tarea3;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class TData {
+	
+	private Number yValues;
+	private Number xValues;
+	private int serieIndex;
+	
+	public TData(String xValue, String yValue, String serieIndex) throws IOException {
+		this.xValues = Integer.parseInt(xValue);
+		this.yValues = Integer.parseInt(yValue);
+		this.serieIndex = Integer.parseInt(serieIndex);
+	}
+	
+	public Number getXValues() {
+		return xValues;
+	}
+
+
+
+	public Number getYValues() {
+		return yValues;
+	}
+	
+	public int getSerieIndex(){
+		return serieIndex;
+	}
 
 }
--- cc3002/src/tarea3/TDataReader.java
+++ cc3002/src/tarea3/TDataReader.java
@@ -1,9 +1,32 @@
 package tarea3;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class TDataReader extends DataReader {
 
+	private ArrayList<TData> series = new ArrayList<TData>();
+	
+	public ArrayList<Number> getXAxis(int index) {
+		ArrayList<Number> xAxis =  new ArrayList<Number>();
+		for (TData xValue : series ) {
+			if(xValue.getSerieIndex()==index){
+				xAxis.add(xValue.getXValues());
+			}
+			
+		}
+		return xAxis;
+	}
+	
+	public ArrayList<Number> getYAxis(int index) {
+		ArrayList<Number> yAxis =  new ArrayList<Number>();
+		for (TData yValue : series ) {
+			if(yValue.getSerieIndex()==index){
+				yAxis.add(yValue.getXValues());
+			}		
+		}
+		return yAxis;
+	}
 	
 
 }
